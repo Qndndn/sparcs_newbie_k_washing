0332a4a7edc2c6087dffcd402bbf3428462f8c6a
hyojin-0510 <qndndn0510@gmail.com>
2023-08-21 02:01:15 +0900

    세탁시간 설정 전까지

--- k_washing/models.py
+++ k_washing/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
 class k_washing(models.Model):
     floor = models.CharField(max_length=5, choices=(("1", "1층"), ("2", "2층"), ("3", "3층"), ("4", "4층")))
     direction = models.CharField(max_length=5, choices=(("1", "방향1"), ("2", "방향2"), ("3", "방향3"), ("4", "방향4")))
     time = models.IntegerField()
-    content = models.TextField()
+    moretime = IntegerRangeField(min_value=0, max_value=10, null=True)
+    content = models.CharField(max_length=25, choices=( ("없음", "없음"), ("빨래가 끝난 후 바구니에 넣어주세요.", "빨래가 끝난 후 바구니에 넣어주세요."), ("세탁이 빨리 끝날 수 있습니다.", "세탁이 빨리 끝날 수 있습니다."), ("시간이 오래걸립니다.", "시간이 오래걸립니다."), ("향긋한 빨래 되세요:)", "향긋한 빨래 되세요:)"), ("K세탁 폼 미쳤다.", "K세탁 폼 미쳤다.")))
+    finish_time = models.CharField(null=True, max_length=100000)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    state = models.IntegerField()
 
     def __str__(self):
         return f'[{self.pk}] {self.author} :: {self.floor} :: {self.direction} :: {self.time} :: {self.content}'
--- k_washing/views.py
+++ k_washing/views.py
@@ -3,30 +3,64 @@ from django.views.generic import ListView, CreateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import k_washing
 from .forms import k_washingForm
+from datetime import datetime
 
 # Create your views here.
 class k_washingList(ListView):
     model = k_washing
-    fields = ['floor','direction']
+    fields = ['time','floor','direction', 'finish_time', "state"]
 
     def get(self, request):
         if request.method == 'GET':
-            print("safjkfjlsdfjkllsd")
             form = k_washingForm(request.GET)
-            print(form.is_bound)
-            print(form.errors)
+            #print(form.is_bound)
+            #print(form.errors)
             if form.is_valid():
                 print("is valid")
                 # form.save()
                 floor_ = form.cleaned_data.get('floor')
                 direction_ = form.cleaned_data.get('direction')
-                print(floor_)
-                print(direction_)
-                return render(request, 'k_washing/k_washing_list.html', {"k_washing_list2": k_washing.objects.filter(floor = floor_, direction = direction_)})
+                #print(floor_)
+                #print(direction_)
+                k_washing_list2 = k_washing.objects.filter(floor = floor_, direction = direction_, )
+                now = datetime.now()
+                time1=0
+                for i in k_washing_list2:
+                    hour = int(i.finish_time.split(':')[0])
+                    minute= int(i.finish_time.split(':')[1])
+                    i.time = hour*60+minute-now.hour*60-now.minute
+                    time1+=i.time
+
+                minute_=(now.minute+time1)%60
+                hour_=(now.hour+(time1+now.minute)//60)%24
+                time_=str(hour_)+":"+str(minute_)
+
+                #print("self.request.user: ")
+                #print(self.request.user)
+                #print(k_washing_list2[0].author)
+                time_first=0
+                if k_washing_list2:
+                    if self.request.user == k_washing_list2[0].author:
+                        state = 1
+                        #print(11)
+                    time_first = k_washing_list2[0].time
+                else: state = 0
+
+                return render(
+                    request,
+                    'k_washing/k_washing_list.html',
+                    {
+                        "k_washing_list2": k_washing_list2,
+                        "floor_": floor_,
+                        "direction_": direction_,
+                        "time_": time_,
+                        "state": state,
+                        "time_first": time_first
+                    }
+                )
         else:
             form = k_washingForm()
         return render(request, 'k_washing/k_washing_list.html')
-
 
     
 class k_washingCreate(LoginRequiredMixin, CreateView):
@@ -37,6 +71,17 @@ class k_washingCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         current_user = self.request.user
         if current_user.is_authenticated:
+
+            k_washing_list2 = k_washing.objects.filter(floor = form.instance.floor, direction = form.instance.direction, )
+            time1=0
+            for i in k_washing_list2:
+                time1+=i.time
+            time1+=form.instance.time
+            now = datetime.now()
+            minute = (now.minute+time1)%60
+            hour = int(now.hour+int(now.minute+time1)//60)
+
+            form.instance.finish_time= str(hour%12) + ":" + str(minute)
             form.instance.author = current_user
             response = super(k_washingCreate, self).form_valid(form)
 
