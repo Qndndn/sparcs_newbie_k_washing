92adeaa7cf2efaf2fb0eee9fcf6af2d9d6bdbf80
hyojin-0510 <qndndn0510@gmail.com>
2023-08-25 13:22:29 +0900

    세탁종료 버튼

--- k_washing/urls.py
+++ k_washing/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'k_washing'
+
 urlpatterns = [
     path('', views.k_washingList.as_view(), name='k_washingList'),
+    path('delete/<int:pk_1>/', views.k_washing_list_delete, name='k_washing_list_delete'),
     path('create/',views.k_washingCreate.as_view()),
 ]
--- k_washing/views.py
+++ k_washing/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, CreateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import k_washing
 from .forms import k_washingForm
 from datetime import datetime
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 class k_washingList(ListView):
@@ -18,9 +19,9 @@ class k_washingList(ListView):
             #print(form.is_bound)
             #print(form.errors)
             if form.is_valid():
-                #print("is valid")
+                print("is valid")
                 #form.save()
-                #print(form.errors)
+                print(form.errors)
                 floor_ = form.cleaned_data.get('floor')
                 direction_ = form.cleaned_data.get('direction')
                 #print(floor_)
@@ -54,27 +55,22 @@ class k_washingList(ListView):
                 #print("self.request.user: ")
                 #print(self.request.user)
                 #print(k_washing_list2[0].author)
-
-
-
-
+                state = 0
                 if k_washing_list2:
                     #print(k_washing_list2[0])
                     if self.request.user == k_washing_list2[0].author:
                         state = 1
-                        if k_washing_list2[0].time<=0:
-                            print("빨래가 다 되었습니다.")
-                            messages.warning(request, '빨래가 다 되었습니다.')
-                            #print(k_washing_list2[0])
-
                     hour = int(k_washing_list2[0].finish_time.split(':')[0])
                     minute= int(k_washing_list2[0].finish_time.split(':')[1])
                     k_washing_list2[0].time = hour*60+minute-now.hour*60-now.minute
 
-                    if k_washing_list2[0].time <= 0: k_washing_list2[0].delete()
-
+                    if k_washing_list2[0].time <= 0:
+                        messages.warning(request, '빨래가 다 되었습니다.')
+                        k_washing_list2[0].delete()
                     time_first = k_washing_list2[0].time
-                else: state = 0
+                    pk_1 = k_washing_list2[0].pk
+                else:
+                    pk_1 = 0
 
                 return render(
                     request,
@@ -85,7 +81,8 @@ class k_washingList(ListView):
                         "direction_": direction_,
                         "time_": time_,
                         "state": state,
-                        "time_first": time_first
+                        "time_first": time_first,
+                        "pk_1": pk_1
                     }
                 )
             else:
@@ -95,7 +92,35 @@ class k_washingList(ListView):
             form = k_washingForm()
         return render(request, 'k_washing/k_washing_list.html')
 
-    
+
+from django.http import JsonResponse
+
+@login_required(login_url='common:login')
+def k_washing_list_delete(request, pk_1):
+    print("삭제버튼 눌려짐")
+    print("pk_1: ")
+    print(pk_1)
+    try:
+        k_washing_a = get_object_or_404(k_washing, pk=pk_1)
+        print("해치웠나")
+        if request.user != k_washing_a.author:
+            print(request.user)
+            print(k_washing_a.author)
+            messages.error(request, '현재 진행중인 세탁이 없습니다.')
+            return redirect('/')
+        print("뭐임 ?")
+        k_washing_a.delete()
+        
+        # 삭제 후 현재 페이지에 머물도록 JSON 응답 반환
+        if request.is_ajax():
+            return JsonResponse({'deleted': True})
+        return redirect('k_washingList')  # 혹은 적절한 리다이렉션 URL로 변경
+    except: 
+        return redirect('/')
+
+
+
+
 class k_washingCreate(LoginRequiredMixin, CreateView):
     model = k_washing
     fields = ['floor','direction', 'time', 'content']
